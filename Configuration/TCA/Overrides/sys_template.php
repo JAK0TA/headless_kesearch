399991e44acbe9c57814258a55b42649c9a04d26
HardAnders GbR <96169631+hardanders-dev@users.noreply.github.com>
2022-01-08 15:54:56 +0100

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,7 +3,7 @@
 defined('TYPO3') || die();
 
 call_user_func(function () {
-    // Default TypoScript for Headless News
+    // Default TypoScript for Headless Faceted Search (ke_search)
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         'headless_kesearch',
         'Configuration/TypoScript',
