20e215435b0e1f22d43410f68849e27c7a38a746
HardAnders GbR <96169631+hardanders-dev@users.noreply.github.com>
2022-01-08 16:17:22 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,7 @@
 <?php
 
+defined('TYPO3_MODE') or die();
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][Tpwd\KeSearch\Lib\Searchresult::class] = [
     'className' => Friendsoftypo3headless\HeadlessKesearch\XClass\Lib\Searchresult::class
 ];
